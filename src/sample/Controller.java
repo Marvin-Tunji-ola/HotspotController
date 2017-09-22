bac9e49b31c4868235244e5e4fbb41f04321b11c
Marvin <marvinsmart9@gmail.com>
2017-09-22 18:57:14 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,215 @@
+package sample;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import sample.Hotspot;
+
+import javax.swing.*;
+
+
+public class Controller implements Initializable {
+    boolean isActive = false;
+
+
+    @FXML
+    public ToggleButton hotspotSwitch;
+
+    @FXML
+    public Label ssid, type, security, status, connected, errorLabel, successLabel;
+
+    @FXML
+    public TextArea console;
+
+    @FXML public Hyperlink developer;
+
+    @FXML
+    public TextField editSSID;
+
+    @FXML
+    public PasswordField editPasskey;
+
+    @FXML
+    public CheckBox showPasskey;
+
+    @FXML
+    public VBox settingsForm;
+    @FXML
+    public Button saveSettings;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        hotspotSwitch.setDisable(true);
+        setVariables();
+        new Thread(()->{
+            String connected = Hotspot.getNoOfDevices();
+            while (true){
+                if(!connected.equalsIgnoreCase(Hotspot.getNoOfDevices())){
+
+                    Platform.runLater(()->{
+                        this.connected.setText(Hotspot.getNoOfDevices());
+                    });
+                }
+            }
+        }).start();
+
+        developer.setOnAction((e)->{
+            try {
+                Desktop.getDesktop().browse(new URI("http://www.example.com"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (URISyntaxException e1) {
+                e1.printStackTrace();
+            }
+        });
+        hotspotSwitch.setOnAction((e)->{
+            if(!isActive) {
+                hotspotSwitch.setText("Starting Hotspot.....");
+                startNetwork();
+            }else if(isActive){
+                hotspotSwitch.setText("Stopping Hotspot.....");
+                stopNetwork();
+            }
+        });
+        showPasskey.setOnMouseClicked((e)->{
+            if(showPasskey.isSelected()){
+                //show Password
+                String passkey = editPasskey.getText().toString();
+                settingsForm.getChildren().remove(4);
+                TextField editPasskey = new TextField();
+                editPasskey.setText(passkey);
+                settingsForm.getChildren().add(4,editPasskey);
+
+
+            }else{
+                TextField editPasskey = (TextField) settingsForm.getChildren().get(4);
+                String passkey = editPasskey.getText().toString();
+                settingsForm.getChildren().remove(4);
+                this.editPasskey.setText(passkey);
+                settingsForm.getChildren().add(4,this.editPasskey);
+
+
+            }
+            //JOptionPane.showMessageDialog(null, showPasskey.isSelected()+"Show Password Checked");
+        });
+
+        saveSettings.setOnMouseClicked((e)->{
+            String SSID = editSSID.getText().trim();
+            String passkey = editPasskey.getText().trim();
+            if(passkey.length() > 8) {
+                errorLabel.setText("");
+                //String passResult, ssidResult;
+
+                new Thread(()->{
+                    String passResult = Hotspot.setPassword(passkey);
+                    String ssidResult =Hotspot.setSSID(SSID);
+                    Platform.runLater(()->{
+                        if(passResult.equalsIgnoreCase("success") || ssidResult.equalsIgnoreCase("success")){
+                            setVariables();
+                            successLabel.setText("Successfully Changed");
+                            new Thread(() -> {
+                                try {
+                                    Thread.sleep(2000);
+                                    Platform.runLater(()->{
+                                        successLabel.setText("");
+                                    });
+
+                                } catch (InterruptedException ex) {
+                                    ex.printStackTrace();
+                                }
+
+                            }).start();
+                        }
+                    });
+                    Hotspot.startNetwork();
+                }).start();
+
+
+            }else{
+                errorLabel.setText("Passkey must be atleast 8 Characters");
+            }
+
+        });
+
+
+    }
+    
+    public void startNetwork(){
+        new Thread(()->{
+            String result = Hotspot.startNetwork();
+            if(result.equalsIgnoreCase("started")){
+            Platform.runLater(() -> {
+                //Update UI here
+                isActive = true;
+                setVariables();
+                status.setText("Active");
+                hotspotSwitch.setText("Turn Off Hotspot");
+                hotspotSwitch.setStyle("-fx-background-color:#d31e1e;");
+                console.setText("Hosted Network Started");
+            });
+            }else{
+                console.setText(result);
+            }
+        }).start();
+
+
+
+
+    }
+
+    public void stopNetwork(){
+        new Thread(()->{
+        String result = Hotspot.stopNetwork();
+        if(result.equalsIgnoreCase("stopped")){
+            Platform.runLater(() -> {
+            isActive = false;
+            setVariables();
+            status.setText("InActive");
+            hotspotSwitch.setText("Turn On Hotspot");
+            hotspotSwitch.setStyle("-fx-background-color:#0fc944;");
+            console.setText("Hosted Network Stopped");
+            });
+        }else{
+            console.setText(result);
+        }
+        }).start();
+    }
+
+    public void setVariables(){
+
+        new Thread(()->{
+            String ssidString = Hotspot.getSSID(), noOfDev = Hotspot.getNoOfDevices(),
+            securityType = Hotspot.getSecurity(), password = Hotspot.getPassword();
+            isActive = Hotspot.isActive();
+            Platform.runLater(()->{
+                ssid.setText(ssidString);
+                security.setText(securityType);
+                connected.setText(noOfDev);
+                editSSID.setText(ssidString);
+                editPasskey.setText(password);
+                hotspotSwitch.setDisable(false);
+                if(isActive){
+                    status.setText("Active");
+                    hotspotSwitch.setText("Turn Off Hotspot");
+                    hotspotSwitch.setStyle("-fx-background-color:#d31e1e;");
+                    console.setText("Hosted Network Started");
+                }
+            });
+        }).start();
+
+    }
+
+}
---
+++ src/sample/Hotspot.java
@@ -0,0 +1,253 @@
+package sample;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Created by Marvin on 9/20/2017.
+ */
+public class Hotspot {
+
+    public static String startNetwork(){
+
+        String input = "";
+        String output;
+        try{
+            Process p = Runtime.getRuntime().exec("netsh wlan start hostednetwork");
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            Scanner in = new Scanner(stdInput);
+            while (in.hasNextLine()){
+                input += in.nextLine();
+            }
+            if(input.trim().equalsIgnoreCase("The hosted network started.")){
+                output = "started";
+            }else{
+                output = "Error: \n "+input;
+            }
+
+        }catch(IOException ex){
+            output = ex.getMessage().toString();
+        }
+        return output;
+    }
+
+    public static String stopNetwork(){
+        String input = "";
+        String output;
+        try{
+            Process p = Runtime.getRuntime().exec("netsh wlan stop hostednetwork");
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Scanner in = new Scanner(stdInput);
+
+            while (in.hasNextLine()){
+                input += in.nextLine();
+            }
+
+            if(input.trim().equalsIgnoreCase("The hosted network stopped.")){
+                output = "stopped";
+            }else{
+                output = "Error: \n "+input;
+            }
+
+        }catch(IOException ex){
+            output = ex.getMessage().toString();
+        }
+        return output;
+    }
+
+    public static String setSSID(String SSID){
+        String input = "";
+        String output;
+        try{
+            Process p = Runtime.getRuntime().exec("netsh wlan set hostednetwork ssid = "+SSID);
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            Scanner in = new Scanner(stdInput);
+            while (in.hasNextLine()){
+                input += in.nextLine();
+            }
+            if(input.trim().equalsIgnoreCase("The SSID of the hosted network has been successfully changed.")){
+                output = "success";
+            }else{
+                output = "Error: \n "+input;
+            }
+
+        }catch(IOException ex){
+            JOptionPane.showMessageDialog(null,ex.getMessage().toString());
+            output = "null";
+        }
+        return output;
+
+    }
+
+    public static String getSSID(){
+
+        String output = "";
+        try {
+            Process p = Runtime.getRuntime().exec("netsh wlan show hostednetwork");
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Scanner in = new Scanner(stdInput);
+
+            while (in.hasNext()) {
+                String temp = in.next();
+                if (temp.equalsIgnoreCase("SSID")) {
+                    in.next();
+                    in.next();
+                    output = in.next();
+                }
+            }
+        }catch(IOException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage().toString());
+            output = null;
+        }
+        output = output.replace("\"","");
+        return output;
+    }
+
+    public  static String setPassword(String passkey){
+        String input = "";
+        String output;
+        try{
+            Process p = Runtime.getRuntime().exec("netsh wlan set hostednetwork key = \""+passkey+"\"");
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+            Scanner in = new Scanner(stdInput);
+            while (in.hasNextLine()){
+                input += in.nextLine();
+            }
+            if(input.trim().equalsIgnoreCase("The user key passphrase of the hosted network has been successfully changed.")){
+
+                output = "success";
+
+            }else{
+                output = "Error: \n "+input;
+            }
+
+        }catch(IOException ex){
+            JOptionPane.showMessageDialog(null,ex.getMessage().toString());
+            output = "null";
+        }
+        return output;
+    }
+
+    public static String getPassword(){
+        String output = "";
+        try {
+            Process p = Runtime.getRuntime().exec("netsh wlan show hostednetwork security");
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Scanner in = new Scanner(stdInput);
+
+            while (in.hasNext()) {
+                String temp = in.next();
+                if (temp.equals("User")) {
+                    in.next();
+                    in.next();
+                    in.next();
+                    output = in.next();
+                    break;
+                }
+            }
+        }catch(IOException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage().toString());
+            output = "null";
+        }
+
+        return output;    }
+
+    public static String getSecurity(){
+        String output = "";
+        try {
+            Process p = Runtime.getRuntime().exec("netsh wlan show hostednetwork");
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Scanner in = new Scanner(stdInput);
+
+            while (in.hasNext()) {
+                String temp = in.next();
+                if (temp.equalsIgnoreCase("Authentication")) {
+                    in.next();
+                    output = in.next();
+                }
+            }
+        }catch(IOException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage().toString());
+            output = "null";
+        }
+
+        return output;
+    }
+
+    public static String getNoOfDevices(){
+        String output = "0";
+        try {
+            Process p = Runtime.getRuntime().exec("netsh wlan show hostednetwork");
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Scanner in = new Scanner(stdInput);
+
+            while (in.hasNext()) {
+                String temp = in.next();
+                if (temp.equals("Number")) {
+                    in.next();
+                    in.next();
+                    in.next();
+                    output = in.next();
+                }
+            }
+        }catch(IOException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage().toString());
+            output = "null";
+        }
+
+
+        return output;
+    }
+
+    public static boolean isActive(){
+        boolean isActive = false;
+        String status = "";
+        try {
+            Process p = Runtime.getRuntime().exec("netsh wlan show hostednetwork");
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Scanner in = new Scanner(stdInput);
+
+            while (in.hasNext()) {
+                String temp = in.next();
+                if (temp.equals("Status")) {
+                    in.next();
+                    status = in.next();
+                }
+            }
+            if(status.equals("Started")){
+                isActive = true;
+            }else{
+                isActive = false;
+            }
+        }catch(IOException ex) {
+
+            JOptionPane.showMessageDialog(null, ex.getMessage().toString());
+            isActive = false;
+        }
+
+
+        return isActive;
+
+    }
+
+
+    }
+
+
+
+
+
+
